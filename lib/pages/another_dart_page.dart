13ed9e1076f66f7ff635a8b2dd428816f2469622
Adrita Rahman Tory <45292117+sadgirltory@users.noreply.github.com>
2020-06-29 02:04:29 +0600

    commit

---
+++ lib/pages/another_dart_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AnotherPage extends StatefulWidget {
+  @override
+  _AnotherPageState createState() => _AnotherPageState();
+}
+
+class _AnotherPageState extends State<AnotherPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
